563c7bf2eefe1da208d6da90e3aaa4cbf99d12da
Oleh Demchenko <olegdemchenko1993@gmail.com>
2022-11-08 18:20:26 +0200

    Add 'tasks' path segment to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 const origin = "http://localhost:5000";
 
 export const routes = {
-  tasks: () => origin,
-  task: (taskId: string) => [origin, taskId].join("/"),
+  tasks: () => [origin, "tasks"].join("/"),
+  task: (taskId: string) => [origin, "tasks", taskId].join("/"),
 };
